40e9a896676329100884b302ade166cadc69ba70
MrThieff <net-thief@ya.ru>
2018-10-01 16:33:26 +0300

    Update get-filepaths-from-since-db.sh

--- get-filepaths-from-since-db.sh
+++ get-filepaths-from-since-db.sh
@@ -6,3 +6,5 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     FILE=$(debugfs -R "ncheck $INODE" $SOURCE /dev/vda1 >&1 2>/dev/null | grep $INODE)
     echo "$INODE: $FILE"
 done < "$1"
+
+#example: sh get-filepaths-from-since-db.sh /var/lib/logstash/.sincedb_6f594c2c64a2444a5dae74dfc2e1d1a2 /dev/vda1
